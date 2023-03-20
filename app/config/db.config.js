45e940bbde15f985711c71f53742c68cd2bec719
sejalpatel125 <91778994+sejalpatel125@users.noreply.github.com>
2023-03-20 22:21:40 +0530

    Add files via upload

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://sejalpatel125:eGyvStxzMlS2lLVP@cluster0.nbshmi7.mongodb.net/SMS_DB"
+};
---
+++ app/controllers/student.controller.js
@@ -0,0 +1,169 @@
+const db = require("../models");
+const Student = db.students;
+
+// Create and Save a new Student
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.email) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
+  // Create a Student
+  const student = new Student({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    profilePicture: req.body.profilePicture,
+    DOB: new Date(req.body.DOB).getTime(),
+    gender: req.body.gender,
+    mobileNo: req.body.mobileNo,
+    email: req.body.email,
+    departments: req.body.departments,
+    status: req.body.status || true
+  });
+
+  // Save Student in the database
+  student
+    .save(student)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Student."
+      });
+    });
+};
+
+// Retrieve all Students from the database.
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? {
+    $or: [{ firstName: { $regex: new RegExp(title), $options: "i" } },
+    { lastName: { $regex: new RegExp(title), $options: "i" } },
+    { email: { $regex: new RegExp(title), $options: "i" } },
+    { mobileNo: { $regex: new RegExp(title), $options: "i" } },
+    ]
+  } : {};
+  if (req.query.DOB) {
+
+    const startDate = req.query.DOB.split(',')[0];
+    const endDate = req.query.DOB.split(',')[0];
+    console.log(startDate)
+    condition = {
+      DOB: {
+        $gt: startDate,
+        $lt: endDate
+      }
+    }
+  }
+  if (req.query.gender){
+    condition = req.query.gender > 0 ? {gender:{$eq: req.query.gender}} : {}
+  }
+  Student.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving students."
+      });
+    });
+};
+
+// Find a single Student with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Student.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Student with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Student with id=" + id });
+    });
+};
+
+// Update a Student by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Student.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Student with id=${id}. Maybe Student was not found!`
+        });
+      } else res.send({ message: "Student was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Student with id=" + id
+      });
+    });
+};
+
+// Delete a Student with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Student.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Student with id=${id}. Maybe Student was not found!`
+        });
+      } else {
+        res.send({
+          message: "Student was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Student with id=" + id
+      });
+    });
+};
+
+// Delete all Students from the database.
+exports.deleteAll = (req, res) => {
+  Student.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Students were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all students."
+      });
+    });
+};
+
+// Find all published Students
+exports.findAllPublished = (req, res) => {
+  Student.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving students."
+      });
+    });
+};
